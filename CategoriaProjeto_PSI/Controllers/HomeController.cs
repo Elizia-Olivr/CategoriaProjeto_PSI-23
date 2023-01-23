9ee3944d95383682dae29471def3d239a2188a0f
Elízia Regina de Oliveira Silva <elizia.regina@academico.ifrn.edu.br>
2023-01-23 07:46:32 -0300

    Adicionar arquivos de projeto.

---
+++ CategoriaProjeto_PSI/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Servico.Cadastros;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CategoriaProjeto_PSI.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        private ProdutoServico produtoServico = new ProdutoServico();
+        public ActionResult Index()
+        {
+            return View(produtoServico.ObterProdutosClassificadosPorNome());
+        }
+    }
+}
